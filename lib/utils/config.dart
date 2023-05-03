591d62fea4def67392b0940481906dfb871476e4
Carl Nieves <solox312@gmail.com>
2023-05-02 22:36:21 -0400

    updated version

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -53,7 +53,7 @@ const kTextSmallHeaderStyle = TextStyle(
   overflow: TextOverflow.ellipsis,
 );
 
-const String currentAppVersion = '1.0.2-dev';
+const String currentAppVersion = '1.0.3-dev';
 
 const kTextHeaderStyle = TextStyle(
   fontFamily: 'PoppinsSB',
