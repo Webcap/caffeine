daff909140040d993b10204b9e5295879b86ec7f
Carl Nieves <solox312@gmail.com>
2026-06-25 19:16:49 -0400

    Use flutter_app_installer for APK installs
    Replace install_plugin_v2 with flutter_app_installer: FileOpener now uses FlutterAppInstaller.installApk(filePath:), removed PackageInfo/InstallPlugin usage. Updated pubspec.yaml and pubspec.lock to add flutter_app_installer and remove install_plugin_v2. AndroidManifest updated to register FileProvider and added res/xml/file_paths.xml. Also includes a generated Gradle problems-report.html.

--- lib/services/file_opener_service.dart
+++ lib/services/file_opener_service.dart
@@ -1,13 +1,13 @@
 import 'package:open_file/open_file.dart';
-import 'package:install_plugin_v2/install_plugin_v2.dart';
-import 'package:package_info_plus/package_info_plus.dart';
+import 'package:flutter_app_installer/flutter_app_installer.dart';
 import 'dart:io';
 
 class FileOpener {
+  final FlutterAppInstaller _appInstaller = FlutterAppInstaller();
+
   Future<dynamic> open(String path, {String? type}) async {
     if (Platform.isAndroid && path.toLowerCase().endsWith('.apk')) {
-      final info = await PackageInfo.fromPlatform();
-      return InstallPlugin.installApk(path, info.packageName);
+      return _appInstaller.installApk(filePath: path);
     }
     return OpenFile.open(path, type: type);
   }
